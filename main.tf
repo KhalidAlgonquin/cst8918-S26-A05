655e76b1bb5ebbe1effa691623b526acd27ee1b8
KhalidAlgonquin <amch0001@algonquinlive.com>
2026-06-15 10:13:50 -0400

    Add subnet for web server

--- main.tf
+++ main.tf
@@ -77,3 +77,14 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
+
+# -----------------------------
+# Subnet
+# -----------------------------
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.labelPrefix}-A05-SUBNET"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
