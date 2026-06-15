52136f98f000902edd5c335aadc874c5536801d5
KhalidAlgonquin <amch0001@algonquinlive.com>
2026-06-15 10:14:33 -0400

    Add security rules for SSH and HTTP

--- main.tf
+++ main.tf
@@ -88,3 +88,39 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+# -----------------------------
+# Network Security Group
+# Allows SSH and HTTP traffic
+# -----------------------------
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.labelPrefix}-A05-NSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
