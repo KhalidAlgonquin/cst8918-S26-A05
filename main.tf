d936737c8ef2e338cefa70fca04cfda2e16241c4
KhalidAlgonquin <amch0001@algonquinlive.com>
2026-06-15 10:24:28 -0400

    Add Ubuntu web server virtual machine

--- main.tf
+++ main.tf
@@ -164,3 +164,39 @@ data "cloudinit_config" "web_server_config" {
     content      = file("${path.module}/init.sh")
   }
 }
+
+# -----------------------------
+# Linux Virtual Machine
+# -----------------------------
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.labelPrefix}-A05-VM"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    name                 = "${var.labelPrefix}-A05-OSDISK"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  custom_data = data.cloudinit_config.web_server_config.rendered
+}
