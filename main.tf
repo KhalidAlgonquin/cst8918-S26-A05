fafa9222c3fa8b8f72c87889bb6b973f2be9ee6f
KhalidAlgonquin <amch0001@algonquinlive.com>
2026-06-15 10:10:54 -0400

    Add Azure resource group

--- main.tf
+++ main.tf
@@ -45,3 +45,12 @@ variable "admin_username" {
   type        = string
   default     = "azureadmin"
 }
+
+# -----------------------------
+# Resource Group
+# -----------------------------
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.labelPrefix}-A05-RG"
+  location = var.region
+}
